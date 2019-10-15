9830ecb2d2f339cc5735b8948c0cd2c987967747
Tamino1762 <40547572+Tamino1762@users.noreply.github.com>
2019-10-14 19:51:27 -0500

    Initial Assignment, no extra credit

---
+++ RaceTrack/Drivers/Potter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    class Potter : Driver
+    {
+        public Potter(RaceCar car) : base(car)
+        {
+            Name = "Potter";
+            SkillLevel = 20;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+    }
+}
